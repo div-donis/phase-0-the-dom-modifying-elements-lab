18864637696b1799604438551d3af782f75ca614
Donovan <divdonis@gmail.com>
2021-04-11 16:33:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let el = document.getElementById('main');
+    el.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = "Div is the champion"
+document.body.appendChild(newHeader);
